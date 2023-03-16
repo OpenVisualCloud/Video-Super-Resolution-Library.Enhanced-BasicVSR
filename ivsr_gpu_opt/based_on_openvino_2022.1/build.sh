3622a3627d79d0aadf945a8bcc372da8ad32d6a7
windy058 <yang.zheng@intel.com>
2023-03-16 13:25:11 +0800

    Add patches to support basicVSR in OpenVINO. (#26)
    * Restructure master branch to make it accomodate three projects: VSR GPU Opt, VSR FFmpeg plugin, and VSR SDK.
    
    Signed-off-by: Pan, Yanjie <yanjie.pan@intel.com>
    
    * Rename the folders with lower case letters.
    
    Signed-off-by: Pan, Yanjie <yanjie.pan@intel.com>
    
    * Add openvino as submodule and checkout to specific version.
    
    Signed-off-by: Pan, Yanjie <yanjie.pan@intel.com>
    
    * Add patches to support basicVSR in OpenVINO.
    
    Signed-off-by: yangz1 <yang.zheng@intel.com>
    
    * Update build.sh for OV22.1 openvino.
    
    Signed-off-by: yangz1 <yang.zheng@intel.com>
    
    * Default disable GPU plugin in build.sh.
    
    Signed-off-by: yangz1 <yang.zheng@intel.com>
    
    ---------
    
    Signed-off-by: Pan, Yanjie <yanjie.pan@intel.com>
    Signed-off-by: yangz1 <yang.zheng@intel.com>
    Co-authored-by: Pan, Yanjie <yanjie.pan@intel.com>

---
+++ ivsr_gpu_opt/based_on_openvino_2022.1/build.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+EX_PATH=${PWD}
+git submodule update --init ./openvino
+cd openvino
+git submodule update --init --recursive
+git am --whitespace=fix ${EX_PATH}/patches/0001-Vsr-opt-dev-2.patch
+git am --whitespace=fix ${EX_PATH}/patches/0002-remove-the-onednn-folder-by-incorrect-submit-4.patch
+git am --whitespace=fix ${EX_PATH}/patches/0003-BasicVSR-inference-sample-code-in-python-5.patch
+git am --whitespace=fix ${EX_PATH}/patches/0004-flow_warp-custom-op-ocl-implementation-and-applicati.patch
+git am --whitespace=fix ${EX_PATH}/patches/0005-Remove-cached-onednn.-8.patch
+git am --whitespace=fix ${EX_PATH}/patches/0006-Enable-BasicVSR-model-conversion-from-pytorch-to-onn.patch
+git am --whitespace=fix ${EX_PATH}/patches/0007-modification-of-custom-op-for-MO-10.patch
+git am --whitespace=fix ${EX_PATH}/patches/0008-Reorder-optimization-9.patch
+git am --whitespace=fix ${EX_PATH}/patches/0009-remove-debug-information-11.patch
+git am --whitespace=fix ${EX_PATH}/patches/0010-Add-license.-12.patch
+git am --whitespace=fix ${EX_PATH}/patches/0011-Add-nif-width-height-arguments-to-pytorch2onnx.py-to.patch
+git am --whitespace=fix ${EX_PATH}/patches/0012-add-refined-cpp-sample-of-BasicVSR-14.patch
+git am --whitespace=fix ${EX_PATH}/patches/0013-clean-useless-files-and-complement-the-introduction-.patch
+git am --whitespace=fix ${EX_PATH}/patches/0014-add-lisense-header-16.patch
+git am --whitespace=fix ${EX_PATH}/patches/0015-restore-file-mode-to-644-17.patch
+git am --whitespace=fix ${EX_PATH}/patches/0016-Vsr-opt-dev-20.patch
+git am --whitespace=fix ${EX_PATH}/patches/0017-Add-3rd-party-license-and-modify-readme.-21.patch
+if [ ! -d "./build" ];then
+    mkdir -p build
+fi
+cd build
+
+cmake \
+-DCMAKE_INSTALL_PREFIX=${PWD}/../install \
+-DENABLE_INTEL_CPU=ON \
+-DENABLE_CLDNN=OFF \
+-DENABLE_INTEL_GPU=OFF \
+-DENABLE_ONEDNN_FOR_GPU=OFF \
+-DENABLE_INTEL_GNA=OFF \
+-DENABLE_INTEL_MYRIAD_COMMON=OFF \
+-DENABLE_INTEL_MYRIAD=OFF \
+-DENABLE_PYTHON=ON \
+-DENABLE_OPENCV=ON \
+-DENABLE_SAMPLES=ON \
+-DENABLE_CPPLINT=OFF \
+-DTREAT_WARNING_AS_ERROR=OFF \
+-DENABLE_TESTS=OFF \
+-DENABLE_GAPI_TESTS=OFF \
+-DENABLE_BEH_TESTS=OFF \
+-DENABLE_FUNCTIONAL_TESTS=OFF \
+-DENABLE_OV_CORE_UNIT_TESTS=OFF \
+-DENABLE_OV_CORE_BACKEND_UNIT_TESTS=OFF \
+-DCMAKE_BUILD_TYPE=Release ..
+
+make -j`nproc`
+
+make install
+
+echo "Building OV22.1 finished."
