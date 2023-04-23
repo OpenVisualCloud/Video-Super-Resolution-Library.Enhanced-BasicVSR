92eec172546be1b45e891d92c3f1ebc3f1672e17
KunLong9 <105702843+KunLong9@users.noreply.github.com>
2023-04-23 11:03:30 +0800

    Add env requirements for python basicvsr_inference_sample.py (#36)
    * add env requirements for python basicvsr_inference_sample.py
    
    * Modify patch loading method in build.sh.
    
    Signed-off-by: yangz1 <yang.zheng@intel.com>
    
    ---------
    
    Signed-off-by: yangz1 <yang.zheng@intel.com>
    Co-authored-by: yangz1 <yang.zheng@intel.com>

--- ivsr_gpu_opt/based_on_openvino_2022.1/build.sh
+++ ivsr_gpu_opt/based_on_openvino_2022.1/build.sh
@@ -3,24 +3,14 @@ EX_PATH=${PWD}
 git submodule update --init ./openvino
 cd openvino
 git submodule update --init --recursive
-git am --whitespace=fix ${EX_PATH}/patches/0001-Vsr-opt-dev-2.patch
-git am --whitespace=fix ${EX_PATH}/patches/0002-remove-the-onednn-folder-by-incorrect-submit-4.patch
-git am --whitespace=fix ${EX_PATH}/patches/0003-BasicVSR-inference-sample-code-in-python-5.patch
-git am --whitespace=fix ${EX_PATH}/patches/0004-flow_warp-custom-op-ocl-implementation-and-applicati.patch
-git am --whitespace=fix ${EX_PATH}/patches/0005-Remove-cached-onednn.-8.patch
-git am --whitespace=fix ${EX_PATH}/patches/0006-Enable-BasicVSR-model-conversion-from-pytorch-to-onn.patch
-git am --whitespace=fix ${EX_PATH}/patches/0007-modification-of-custom-op-for-MO-10.patch
-git am --whitespace=fix ${EX_PATH}/patches/0008-Reorder-optimization-9.patch
-git am --whitespace=fix ${EX_PATH}/patches/0009-remove-debug-information-11.patch
-git am --whitespace=fix ${EX_PATH}/patches/0010-Add-license.-12.patch
-git am --whitespace=fix ${EX_PATH}/patches/0011-Add-nif-width-height-arguments-to-pytorch2onnx.py-to.patch
-git am --whitespace=fix ${EX_PATH}/patches/0012-add-refined-cpp-sample-of-BasicVSR-14.patch
-git am --whitespace=fix ${EX_PATH}/patches/0013-clean-useless-files-and-complement-the-introduction-.patch
-git am --whitespace=fix ${EX_PATH}/patches/0014-add-lisense-header-16.patch
-git am --whitespace=fix ${EX_PATH}/patches/0015-restore-file-mode-to-644-17.patch
-git am --whitespace=fix ${EX_PATH}/patches/0016-Vsr-opt-dev-20.patch
-git am --whitespace=fix ${EX_PATH}/patches/0017-Add-3rd-party-license-and-modify-readme.-21.patch
-git am --whitespace=fix ${EX_PATH}/patches/0001-Bug-Fix-Modified-C-warp-extension-implementation-to-.patch
+
+cp ../patches/*.patch ./
+for patch_file in $(find -iname "*.patch" | sort -n);
+do
+    echo "Applying: ${patch_file}";
+    git am --whitespace=fix ${patch_file};
+done
+
 if [ ! -d "./build" ];then
     mkdir -p build
 fi
